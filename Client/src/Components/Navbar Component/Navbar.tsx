2cf2306477fff37e001646e0ce76706de229669c
Mayank <mayank@Mayanks-MacBook-Air.local>
2026-05-19 13:10:09 +0530

    Added Loading Components

--- Client/src/Components/Navbar Component/Navbar.tsx
+++ Client/src/Components/Navbar Component/Navbar.tsx
@@ -1,7 +1,7 @@
 
 export default function Navbar() {
   return (
-    <div className="w-full p-7 flex justify-left bg-neutral-950 border-b border-white/20">
+    <div className="w-full p-7 flex justify-left bg-slate-950 border-b border-white/20">
         <h1 className="text-2xl cursor-pointer text-white font-body font-extrabold  ">FitTrakk</h1>
     </div>
   )
